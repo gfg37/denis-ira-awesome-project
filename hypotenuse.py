7ded7198c2f3184d8dd1e43acc6a151a5380886b
Denis Ira <idenpol@mail.ru>
2025-12-04 12:08:39 +0300

    Add area calculation with intentional error

--- hypotenuse.py
+++ hypotenuse.py
@@ -1,4 +1,7 @@
 import math
+def get_area(a, b):
+    return a * b  # тут намеренная ошибка — нет деления на 2
+
 def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
@@ -8,3 +11,4 @@ if __name__ == '__main__':
     print('Введите b:')
     b = int(input())
     print('c =', get_hypotenuse(a,b))
+    print('S =', get_area(a,b))
